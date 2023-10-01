886e529461369e7603703f4104f31f5b284fed3e
annKimani-ICS <ann.wangari@strathmore.edu>
2023-10-01 22:03:37 +0300

    Including a drugs catalogue in the drug dispensing tool

---
+++ catalogue/data.php
@@ -0,0 +1,126 @@
+
+<?php $catalog = [];
+// Medicines
+$catalog[401] = [
+    "title" => "Aspirin",
+    "img" => "img/medicines/aspirin.jpg",
+    "category" => "Painkillers",
+    "active_ingredient" => "Acetylsalicylic Acid",
+    "dosage_form" => "Tablet",
+    "strength" => "500 mg",
+    "manufacturer" => "Bayer",
+];
+$catalog[402] = [
+    "title" => "Ibuprofen",
+    "img" => "img/medicines/ibuprofen.jpg",
+    "category" => "Painkillers",
+    "active_ingredient" => "Ibuprofen",
+    "dosage_form" => "Capsule",
+    "strength" => "200 mg",
+    "manufacturer" => "Advil",
+];
+$catalog[403] = [
+    "title" => "Paracetamol",
+    "img" => "img/medicines/paracetamol.jpg",
+    "category" => "Painkillers",
+    "active_ingredient" => "Paracetamol",
+    "dosage_form" => "Tablet",
+    "strength" => "500 mg",
+    "manufacturer" => "Tylenol",
+];
+$catalog[404] = [
+    "title" => "Naproxen",
+    "img" => "img/medicines/naproxen.jpg",
+    "category" => "Painkillers",
+    "active_ingredient" => "Naproxen",
+    "dosage_form" => "Tablet",
+    "strength" => "220 mg",
+    "manufacturer" => "Aleve",
+];
+
+$catalog = [];
+// Medicines for Antibiotics
+$catalog[401] = [
+    "title" => "Amoxicillin",
+    "img" => "img/medicines/amoxicillin.jpg",
+    "category" => "Antibiotics",
+    "active_ingredient" => "Amoxicillin",
+    "dosage_form" => "Capsule",
+    "strength" => "500 mg",
+    "manufacturer" => "Generic",
+];
+
+$catalog[402] = [
+    "title" => "Ciprofloxacin",
+    "img" => "img/medicines/ciprofloxacin.jpg",
+    "category" => "Antibiotics",
+    "active_ingredient" => "Ciprofloxacin",
+    "dosage_form" => "Tablet",
+    "strength" => "250 mg",
+    "manufacturer" => "Generic",
+];
+
+// Medicines for Immunosuppressants
+$catalog[501] = [
+    "title" => "Tacrolimus",
+    "img" => "img/medicines/tacrolimus.jpg",
+    "category" => "Immunosuppressants",
+    "active_ingredient" => "Tacrolimus",
+    "dosage_form" => "Capsule",
+    "strength" => "1 mg",
+    "manufacturer" => "Prograf",
+];
+
+$catalog[502] = [
+    "title" => "Cyclosporine",
+    "img" => "img/medicines/cyclosporine.jpg",
+    "category" => "Immunosuppressants",
+    "active_ingredient" => "Cyclosporine",
+    "dosage_form" => "Soft Gelatin Capsule",
+    "strength" => "100 mg",
+    "manufacturer" => "Neoral",
+];
+
+// Medicines for Antiparasitic
+$catalog[601] = [
+    "title" => "Mebendazole",
+    "img" => "img/medicines/mebendazole.jpg",
+    "category" => "Antiparasitic",
+    "active_ingredient" => "Mebendazole",
+    "dosage_form" => "Chewable Tablet",
+    "strength" => "100 mg",
+    "manufacturer" => "Vermox",
+];
+
+$catalog[602] = [
+    "title" => "Ivermectin",
+    "img" => "img/medicines/ivermectin.jpg",
+    "category" => "Antiparasitic",
+    "active_ingredient" => "Ivermectin",
+    "dosage_form" => "Tablet",
+    "strength" => "6 mg",
+    "manufacturer" => "Generic",
+];
+
+// Medicines for Antifungal
+$catalog[701] = [
+    "title" => "Fluconazole",
+    "img" => "img/medicines/fluconazole.jpg",
+    "category" => "Antifungal",
+    "active_ingredient" => "Fluconazole",
+    "dosage_form" => "Tablet",
+    "strength" => "150 mg",
+    "manufacturer" => "Diflucan",
+];
+
+$catalog[702] = [
+    "title" => "Ketoconazole",
+    "img" => "img/medicines/ketoconazole.jpg",
+    "category" => "Antifungal",
+    "active_ingredient" => "Ketoconazole",
+    "dosage_form" => "Shampoo",
+    "strength" => "2%",
+    "manufacturer" => "Nizoral",
+];
+
+?>
